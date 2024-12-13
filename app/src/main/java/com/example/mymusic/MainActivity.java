606ff637afea051619b660cfe95a7299e3714f55
Yang Zhang <2287452104@qq.com>
2024-12-13 16:42:20 +0800

    add: 登录逻辑实现

--- app/src/main/java/com/example/mymusic/MainActivity.java
+++ app/src/main/java/com/example/mymusic/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mymusic;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mymusic.dao.UserDao;
 import com.example.mymusic.until.DBUntil;
+import com.example.mymusic.until.Tools;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +33,33 @@ public class MainActivity extends AppCompatActivity {
         Button loginBtn = findViewById(R.id.login_login_btn);
 
         // 执行登录
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountT.getText().toString();
+                String pwd = pwdT.getText().toString();
 
+                // 判断账号和用户名
+                if (account == null || account.equals("")) {
+                    Tools.Toast(MainActivity.this, "请输入账号");
+                } else if (pwd == null || pwd.equals("")) {
+                    Tools.Toast(MainActivity.this, "请输入密码");
+                } else {
+                    // 登录后判断权限
+                    int status = UserDao.isLogin(account, pwd);
+
+                    if (status == 0) {
+                        Tools.Toast(MainActivity.this, "管理员登录");
+                    } else if (status == 1) {
+                        Tools.Toast(MainActivity.this, "用户登录");
+                    } else {
+                        Tools.Toast(MainActivity.this, "数据格式错误");
+                    }
+
+                }
+
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
